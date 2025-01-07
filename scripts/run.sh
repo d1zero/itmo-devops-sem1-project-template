638692fb2936272626169b03cdba02ae81c979cc
Alex <asashek@yandex.ru>
2025-01-07 05:35:51 +0300

    run fix

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 goose -dir db/migrations postgres "postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}" status
+goose -dir db/migrations postgres "postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}" up
 
-./main
+./main &
